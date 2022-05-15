d87a82fbaadfd70722b8d92adce5fc553aca5bbc
anicr2 <101978137+anicr2@users.noreply.github.com>
2022-05-15 23:43:15 +0300

    back to original

--- tex-sum/src/pages/Contact/Contact.js
+++ tex-sum/src/pages/Contact/Contact.js
@@ -6,7 +6,7 @@ function contact() {
         <div className='first'>
             <h1>Welcome to the Contact page!</h1>
             <h2>
-                Contact details shit, damn it worked
+                Contact details shit
             </h2>
         </div>
     )
