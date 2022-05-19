c2c4ae9fe47cb78f0e7456c38c35bd03f71375d0
anicr2 <101978137+anicr2@users.noreply.github.com>
2022-05-19 21:38:49 +0300

    Navbar Linked to all the pages

--- tex-sum/src/components/Navbar/Navbar.js
+++ tex-sum/src/components/Navbar/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import './Nav.css'
-import MenuItems from './MenuItems.js';
 import LoginPage from '../../pages/Login/Login.js'
 
 function Navbar() {
@@ -9,15 +8,18 @@ function Navbar() {
         <nav className="navBar">
             <img src="./logo.png" alt="TexSum Logo" className='navBarLogo' />
             <ul className="navContent">
-                {MenuItems.map((item, index) =>{
-                    return(
-                        <li key={index}>
-                            <a className={item.className} href={item.url}>
-                                {item.title}
-                            </a>
-                        </li>
-                    )
-                })}
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/">How does it work?</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact Us</Link>
+                </li>
+                <li>
+                    <Link to=" ">Service</Link>
+                </li>
             </ul>
             <button className="loginButton" href={LoginPage}>
                 Log In
