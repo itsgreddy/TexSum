605fe95a771075c5935f28e04559e94153d69689
Harsha <81374435+Harshavardhan7678@users.noreply.github.com>
2022-05-23 22:33:21 +0300

    Basic Landing page completed

--- tex-sum/src/components/Navbar/Navbar.js
+++ tex-sum/src/components/Navbar/Navbar.js
@@ -4,37 +4,40 @@ import './Nav.css'
 
 function Navbar() {
     return (
+        <div className="blur">
+            <nav className="navBar">
 
-        <nav className="navBar">
-
-            <button className='logo-button'>
-                <Link to="/" className="nav-logo-link">
-                    <img src="./logo.png" alt="TexSum Logo" className='navBarLogo' />
-                </Link>
-            </button>
-
-            <ul className="navContent">
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/">How does it work?</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact Us</Link>
-                </li>
-                <li>
-                    <Link to=" ">Service</Link>
-                </li>
-            </ul>
-
-            <Link to="/login">
-                <button className="loginButton">
-                    Log In
+                <button className='logo-button'>
+                    <Link to="/" className="nav-logo-link">
+                        <img src="./logo.png" alt="TexSum Logo" className='navBarLogo' />
+                    </Link>
                 </button>
-            </Link>
 
-        </nav>
+                <ul className="navContent">
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/">How does it work?</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact Us</Link>
+                    </li>
+                    <li>
+                        <Link to=" ">Service</Link>
+                    </li>
+                </ul>
+
+                <Link to="/login">
+                    <button className="loginButton">
+                        Log In
+                    </button>
+                </Link>
+
+            </nav>
+        </div>
+
+
     )
 }
 
--- tex-sum/src/components/Summarize/Summarize.js
+++ tex-sum/src/components/Summarize/Summarize.js
@@ -5,9 +5,22 @@ function Summarize() {
     return (
         <div className="sum">
             <h1>SUMMARIZE YOUR CONTENT IN 1-CLICK</h1>
-            <div className="url">
 
+            <div className="contents">
 
+                <div className="container">
+                    <h2>Orginal Content</h2>
+                    <div className="enter">
+                        <textarea cols="90" rows='20' placeholder="Your text content here.." />
+                    </div>
+                </div>
+
+                <div className="container">
+                    <h2>Summarized Content</h2>
+                    <div className="enter">
+                        <textarea cols="90" rows='20' placeholder="Your TexSum content here.." />
+                    </div>
+                </div>
 
             </div>
 
