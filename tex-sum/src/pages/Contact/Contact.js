fad22a3b39dd60e754a5b6c73b5816f479ab5e54
Harsha <81374435+Harshavardhan7678@users.noreply.github.com>
2022-05-27 14:13:55 +0300

    Necessary changes made

--- tex-sum/src/pages/Contact/Contact.js
+++ tex-sum/src/pages/Contact/Contact.js
@@ -1,45 +1,45 @@
-// import React from 'react';
-// import './Contact.css'
+import React from 'react';
+import './Contact.css'
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import styled from "styled-components"
+import styled from "styled-components"
 
-// function contact() {
-//     return (
-//         <div class="wrapper">
-//             <header><h1 class="underline-small">Contact Us</h1></header>
-//             <form action="#">
-//                 <div class="dbl-field">
-//                     <div class="field">
-//                         <input type="text" name="name" placeholder="Enter your name"></input>
-//                         <FontAwesomeIcon icon="fa-solid fa-user" />
-//                     </div>
-//                     <div class="field">
-//                         <input type="text" name="email" placeholder="Enter your email"></input>
-//                         <FontAwesomeIcon icon="fa-solid fa-envelope" />
-//                     </div>
-//                 </div>
-//                 <div class="dbl-field">
-//                     <div class="field">
-//                         <input type="text" name="phone" placeholder="Enter your phone"></input>
-//                         {<FontAwesomeIcon icon="fa-solid fa-phone" />}
-//                     </div>
-//                     <div class="field">
-//                         <input type="text" name="website" placeholder="Enter your website"></input>
-//                         <FontAwesomeIcon icon="fa-solid fa-browser" />
-//                     </div>
-//                 </div>
-//                 <div class="message">
-//                     <textarea placeholder="Write your message" name="message"></textarea>
-//                     <FontAwesomeIcon icon="fa-solid fa-message-dots" />
-//                 </div>
-//                 <div class="button-area">
-//                     <button type="submit">Send Message</button>
-//                     <span></span>
-//                 </div>
-//             </form>
-//         </div>
-//     )
-// }
+function contact() {
+    return (
+        <div class="wrapper">
+            <header><h1 class="underline-small">Contact Us</h1></header>
+            <form action="#">
+                <div class="dbl-field">
+                    <div class="field">
+                        <input type="text" name="name" placeholder="Enter your name"></input>
+                        {/* <FontAwesomeIcon icon="fa-solid fa-user" /> */}
+                    </div>
+                    <div class="field">
+                        <input type="text" name="email" placeholder="Enter your email"></input>
+                        {/* <FontAwesomeIcon icon="fa-solid fa-envelope" /> */}
+                    </div>
+                </div>
+                <div class="dbl-field">
+                    <div class="field">
+                        <input type="text" name="phone" placeholder="Enter your phone"></input>
+                        {/* {<FontAwesomeIcon icon="fa-solid fa-phone" />} */}
+                    </div>
+                    <div class="field">
+                        <input type="text" name="website" placeholder="Enter your website"></input>
+                        {/* <FontAwesomeIcon icon="fa-solid fa-browser" /> */}
+                    </div>
+                </div>
+                <div class="message">
+                    <textarea placeholder="Write your message" name="message"></textarea>
+                    {/* <FontAwesomeIcon icon="fa-solid fa-message-dots" /> */}
+                </div>
+                <div class="button-area">
+                    <button type="submit">Send Message</button>
+                    <span></span>
+                </div>
+            </form>
+        </div>
+    )
+}
 
 
 // const Container = styled.div`
@@ -57,4 +57,4 @@
 //  backdrop-filter: blur(15);
 // `;
 
-// export default contact;
+export default contact;
