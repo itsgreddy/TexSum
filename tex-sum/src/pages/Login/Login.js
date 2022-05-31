9de2010235054c03cf60942880852f8e3640e6a9
Harsha <81374435+Harshavardhan7678@users.noreply.github.com>
2022-05-31 11:26:44 +0300

    Implemented Sawo Labs login

--- tex-sum/src/pages/Login/Login.js
+++ tex-sum/src/pages/Login/Login.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import SawoLogin from '../SawoLogin/SawoLogin';
 import './Login.css'
 
 function login() {
     return (
         <div className='first'>
             <h1>Welcome to the Login page!</h1>
-            <h2>
-                Just do the Login shit
-            </h2>
+            <div className="sawo">
+                <SawoLogin />
+            </div>
         </div>
     )
 }
